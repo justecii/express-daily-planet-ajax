3c2c464ddf0860b3ca33a2a91c6d198b22a6fb4c
justecii <justecii@gmail.com>
2017-09-18 22:58:42 -0700

    delete works, edit doesnt

--- daily_planet_starter/index.js
+++ daily_planet_starter/index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 var partials = require('express-partials'); // https://github.com/publicclass/express-partials
 var bodyParser = require('body-parser');
@@ -42,6 +43,33 @@ app.post('/articles', function(req, res) {
 
 app.get('/about', function(req, res) {
     res.render('about');
+});
+
+
+app.delete('/articles/:id', function(req, res) {
+  var delIndex = req.params.id;
+  delIndex = parseInt(delIndex);
+    articles.splice(delIndex,1);
+  });
+
+// app.get('/articles/:id/edit', function(req,res) {
+//     var index = req.params.id;
+//     res.render('articles/:id/show', {})
+    
+// })
+
+app.get('/articles/edit/:idx', function(req, res) { //express to show route for animals (show one more animal) -- :idx = paramater, which animal to talk about
+    var edit = req.params.idx; //get animals
+    edit = JSON.parse(edit); //parse list of animals
+    var articleIndex = parseInt(req.params.idx); //get array index from url paramater
+    res.render('articles/edit/:idx', {articles: articles[articleIndex], id: articleIndex}); //render page with data of the specified animal
+});
+app.post('/articles/:idx', function(req, res) { //backend -- express route
+    var articles = fs.readFileSync('./index.ejs'); //read animals file
+    articles = JSON.parse(articles); 
+    articles.push(req.params.idx); //push item to animals array
+    fs.writeFileSync('./index.ejs', JSON.stringify(articles)); //save animals to the data.json file //stringify is opposite of JSON.parse
+    res.redirect('/articles/:index'); //redirect to the GET /animals route (index)
 });
 
 app.listen(3000, function() {
